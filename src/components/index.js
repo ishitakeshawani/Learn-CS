eeb5e3a7321f422b6f594aeb763dde4eb8987efd
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-01 11:55:32 +0530

    build sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Navbar } from "./navbar/Navbar";
 export { Category } from "./category/Category";
 export { Footer } from "./footer/Footer";
+export { SideBar } from "./sidebar/SideBar";
--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { SideBar } from '../../components'
 
 export function Explore() {
   return (
     <div className='explore'>
-
+        <SideBar />
+        
     </div>
   )
 }
