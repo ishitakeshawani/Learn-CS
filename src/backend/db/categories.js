0dfb81fd26ac876cd09a6c2ec900c7d41ec512e8
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-01 18:23:58 +0530

    added videos and categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,19 +9,16 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Operating system",
-    image:
-      "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs=",
+    image: "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs="
   },
   {
     _id: uuid(),
     categoryName: "Computer Networks",
-    image:
-      "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs=",
+    image: "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs="
   },
   {
     _id: uuid(),
     categoryName: "Data structures and algorithms",
-    image:
-      "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs=",
+    image: "https://media.istockphoto.com/photos/abstract-circular-particles-background-picture-id1079645426?b=1&k=20&m=1079645426&s=170667a&w=0&h=AxkETcLGWMDlcU-FayHcJKAFm3DZdYOaOsYsVAe06Cs="
   },
 ];
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -6,16 +6,62 @@
 export const videos = [
   {
     _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    title: "Operating System Full Course",
+    views: "1.2M",
+    duration: "3:35:03",
+    name: "DR Murphy",
+    category: "Operating system",
+    image:
+      "https://i.ytimg.com/vi/mXw9ruZaxzQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDlZqPsL65bkVRc8D-YE3C-4eViAg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "An operating system is system software that manages computer hardware and software resources and provides common services for computer programs. In this operating system full course you will be learning following topic in details.",
   },
   {
     _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    title: "Introduction to Operating System",
+    views: "1.3M",
+    duration: "16:44",
+    name: "Neso Academy",
+    category: "Operating system",
+    image:
+      "https://i.ytimg.com/vi/vBURTt97EkA/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA0Of3wjZg0Mv_XRsqcHkf49mXQqg",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "OS: Introduction to Operating System,What is an Operating System (OS), Types of Operating System (OS), Functions of Operating System (OS),Goals of Operating System (OS)",
+  },
+  {
+    _id: "Lh6id6U2vk5",
+    title: "Computer Networking Full Course",
+    views: "186K",
+    duration: "4:06:56",
+    name: "Kunal Kushwaha",
+    category: "Computer Networks",
+    image:
+      "https://i.ytimg.com/vi/IPvYjXCsTg8/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIceZ7haTqABf21UwHs9gucwnPMw",
+    description:
+      "Learn how the internet works in this complete computer networking course. Here we cover the fundamentals of networking, OSI model deep dive, networking protocols, devices, tools, and more with real-life examples.",
+  },
+  {
+    _id: "U2jsc_8sHv9",
+    title: "Computer Networking Course",
+    views: "1.5M",
+    duration: "9:24:49",
+    name: "freeCodeCamp.org",
+    category: "Computer Networks",
+    image:
+      "https://i.ytimg.com/vi/qiQR5rTSshw/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBF04OGpkE74KJI4Ooyak-MYC4CFg",
+    description:
+      "This full college-level computer networking course will prepare you to configure, manage, and troubleshoot computer networks. It will also help you prepare for CompTIA's Network+ exam.",
+  },
+  {
+    _id: "oOaQVvNK4EU",
+    title: "Data Structures Easy to Advanced Course",
+    views: "4.1M",
+    duration: "8:03:17",
+    name: "freeCodeCamp.org",
+    category: "Data structures and algorithms",
+    image:
+      "https://i.ytimg.com/vi/RBSGKlAvoiM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBfP8ZzayjBgDC-WHYP2cwPWGRrGw",
+    description:
+      "Learn and master the most common data structures in this full course from Google engineer William Fiset. This course teaches data structures to beginners using high quality animations to represent the data structures visually.",
   },
 ];
