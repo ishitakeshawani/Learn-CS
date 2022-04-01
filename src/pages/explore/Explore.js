486b241589544fa64676442c893985ef4fd89415
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-01 17:52:27 +0530

    replace backend files with video lib backend

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import { SideBar } from '../../components'
+import React from "react";
+import { SideBar } from "../../components";
+import { categories } from "../../backend/db/categories";
+import "./explore.css";
+import { products } from "../../backend/db/products";
 
 export function Explore() {
   return (
-    <div className='explore'>
-        <SideBar />
-        
+    <div className="explore-page">
+      <SideBar />
+      <div className="video-section">
+      <div className="category-filter-list">
+      <div className="category-item">All</div>
+      {categories.map((category) => (
+        <div className="category-item">{category.categoryName}</div>
+      ))}
+      </div>
+      <div className="video-list-section">
+        {
+          products.map((product) =>(
+            <div>{product.title}</div>
+          ))
+        }
+      </div>
+      </div>
     </div>
-  )
+  );
 }
