ffe9701d0a01062b1c15082c7c25c8011c1e0f7d
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-02 01:36:46 +0530

    implemented filter by categoryfor videos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { VideoProvider } from "./contexts";
 
 // Call make Server
 makeServer();
@@ -10,7 +11,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <VideoProvider>
+        <App />
+      </VideoProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
--- src/server.js
+++ src/server.js
@@ -4,55 +4,73 @@ import {
   signupHandler,
 } from "./backend/controllers/AuthController";
 import {
-  addItemToCartHandler,
-  getCartItemsHandler,
-  removeItemFromCartHandler,
-  updateCartItemHandler,
-} from "./backend/controllers/CartController";
+  getHistoryVideosHandler,
+  addVideoToHistoryHandler,
+  removeVideoFromHistoryHandler,
+  clearHistoryHandler,
+} from "./backend/controllers/HistoryController";
+import {
+  getAllVideosHandler,
+  getVideoHandler,
+} from "./backend/controllers/VideoController";
+import { videos } from "./backend/db/videos";
+import { categories } from "./backend/db/categories";
 import {
   getAllCategoriesHandler,
   getCategoryHandler,
 } from "./backend/controllers/CategoryController";
 import {
-  getAllProductsHandler,
-  getProductHandler,
-} from "./backend/controllers/ProductController";
+  getLikedVideosHandler,
+  addItemToLikedVideos,
+  removeItemFromLikedVideos,
+} from "./backend/controllers/LikeController";
 import {
-  addItemToWishlistHandler,
-  getWishlistItemsHandler,
-  removeItemFromWishlistHandler,
-} from "./backend/controllers/WishlistController";
-import { categories } from "./backend/db/categories";
-import { products } from "./backend/db/products";
+  getAllPlaylistsHandler,
+  addNewPlaylistHandler,
+  removePlaylistHandler,
+  getVideosFromPlaylistHandler,
+  addVideoToPlaylistHandler,
+  removeVideoFromPlaylistHandler,
+} from "./backend/controllers/PlaylistController";
 import { users } from "./backend/db/users";
-
+import {
+  addItemToWatchLaterVideos,
+  getWatchLaterVideosHandler,
+  removeItemFromWatchLaterVideos,
+} from "./backend/controllers/WatchLaterController";
 export function makeServer({ environment = "development" } = {}) {
   return new Server({
     serializers: {
       application: RestSerializer,
     },
     environment,
+    // TODO: Use Relationships to have named relational Data
     models: {
-      product: Model,
+      video: Model,
       category: Model,
       user: Model,
-      cart: Model,
-      wishlist: Model,
+      like: Model,
+      history: Model,
+      playlist: Model,
+      watchlater: Model,
     },
 
     // Runs on the start of the server
     seeds(server) {
-      // disballing console logs from Mirage
       server.logging = false;
-      products.forEach((item) => {
-        server.create("product", { ...item });
+      videos.forEach((item) => {
+        server.create("video", { ...item });
       });
-
-      users.forEach((item) =>
-        server.create("user", { ...item, cart: [], wishlist: [] })
-      );
-
       categories.forEach((item) => server.create("category", { ...item }));
+      users.forEach((item) =>
+        server.create("user", {
+          ...item,
+          likes: [],
+          watchlater: [],
+          history: [],
+          playlists: [],
+        })
+      );
     },
 
     routes() {
@@ -61,30 +79,58 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/auth/signup", signupHandler.bind(this));
       this.post("/auth/login", loginHandler.bind(this));
 
-      // products routes (public)
-      this.get("/products", getAllProductsHandler.bind(this));
-      this.get("/products/:productId", getProductHandler.bind(this));
+      // video routes (public)
+      this.get("/videos", getAllVideosHandler.bind(this));
+      this.get("video/:videoId", getVideoHandler.bind(this));
+
+      // TODO: POST VIDEO TO DB
 
       // categories routes (public)
       this.get("/categories", getAllCategoriesHandler.bind(this));
       this.get("/categories/:categoryId", getCategoryHandler.bind(this));
 
-      // cart routes (private)
-      this.get("/user/cart", getCartItemsHandler.bind(this));
-      this.post("/user/cart", addItemToCartHandler.bind(this));
-      this.post("/user/cart/:productId", updateCartItemHandler.bind(this));
+      // likes routes (private)
+      this.get("/user/likes", getLikedVideosHandler.bind(this));
+      this.post("/user/likes", addItemToLikedVideos.bind(this));
+      this.delete("/user/likes/:videoId", removeItemFromLikedVideos.bind(this));
+
+      // watch later routes (private)
+      this.get("/user/watchlater", getWatchLaterVideosHandler.bind(this));
+      this.post("/user/watchlater", addItemToWatchLaterVideos.bind(this));
       this.delete(
-        "/user/cart/:productId",
-        removeItemFromCartHandler.bind(this)
+        "/user/watchlater/:videoId",
+        removeItemFromWatchLaterVideos.bind(this)
       );
 
-      // wishlist routes (private)
-      this.get("/user/wishlist", getWishlistItemsHandler.bind(this));
-      this.post("/user/wishlist", addItemToWishlistHandler.bind(this));
+      // playlist routes (private)
+      this.get("/user/playlists", getAllPlaylistsHandler.bind(this));
+      this.post("/user/playlists", addNewPlaylistHandler.bind(this));
       this.delete(
-        "/user/wishlist/:productId",
-        removeItemFromWishlistHandler.bind(this)
+        "/user/playlists/:playlistId",
+        removePlaylistHandler.bind(this)
       );
+
+      this.get(
+        "/user/playlists/:playlistId",
+        getVideosFromPlaylistHandler.bind(this)
+      );
+      this.post(
+        "/user/playlists/:playlistId",
+        addVideoToPlaylistHandler.bind(this)
+      );
+      this.delete(
+        "/user/playlists/:playlistId/:videoId",
+        removeVideoFromPlaylistHandler.bind(this)
+      );
+
+      // history routes (private)
+      this.get("/user/history", getHistoryVideosHandler.bind(this));
+      this.post("/user/history", addVideoToHistoryHandler.bind(this));
+      this.delete(
+        "/user/history/:videoId",
+        removeVideoFromHistoryHandler.bind(this)
+      );
+      this.delete("/user/history/all", clearHistoryHandler.bind(this));
     },
   });
 }
