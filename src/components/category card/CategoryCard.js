957bda8f7a451645c47f7a20d5bef0e9f861c13e
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-02 14:41:25 +0530

    showed selected category on video listing

--- src/components/category card/CategoryCard.js
+++ src/components/category card/CategoryCard.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useVideo } from "../../contexts";
 
-export function CategoryCard({category}) {
+export function CategoryCard({ category }) {
+  const { videoDispatch } = useVideo();
   return (
     <div className="card">
       <div className="card-section regular-font-weight" id="card-section">
-        <img className="card-img" src={category.image} alt="category" />
+        <Link
+          to="/explore"
+          onClick={() =>
+            videoDispatch({
+              type: "SET_SELECTED_CATEGORY",
+              payload: category.categoryName,
+            })
+          }
+        >
+          <img className="card-img" src={category.image} alt="category" />
+        </Link>
         <div className="card-text-overlay">
           <div className="card-header-title bold-font-weight">
             {" "}
