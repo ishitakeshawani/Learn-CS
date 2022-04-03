8b50f715abd19accdb423f2d87be6f558157cfa7
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-04-03 15:35:07 +0530

    build signup  page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from "./homepage/HomePage";
 export { Explore } from "./explore/Explore";
 export { PlayLists } from "./playlists/PlayLists";
 export { Login } from "./login/Login";
+export { SignUp } from "./signup/SignUp";
